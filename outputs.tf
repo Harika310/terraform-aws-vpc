de88e580a5ffc93e247509fab88564c0d141fd93
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-25 15:49:37 -0700

    vpc

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,17 @@ output "vpc_id" {
   
 }
 
+output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
 # output "aws_availability_zones" {
 #     value = data.aws_availability_zones.available
 # }
