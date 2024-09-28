f53cffa414f471334dc73d73552640f9bfbbbdab
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-27 19:42:06 -0700

    terraform -vpc

--- peering.tf
+++ peering.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_peering_connection" "peering" {
 
 # route public peering
 resource "aws_route" "public_peering" {
-    count = var.is_peering_required ? 1: 0
+    count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
@@ -25,7 +25,7 @@ resource "aws_route" "public_peering" {
 
 # route private peering
 resource "aws_route" "private_peering" {
-    count = var.is_peering_required ? 1: 0
+    count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
@@ -33,15 +33,16 @@ resource "aws_route" "private_peering" {
 
 # route private peering
 resource "aws_route" "database_peering" {
-    count = var.is_peering_required ? 1: 0
+    count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 
 resource "aws_route" "default_peering" {
-    count = var.is_peering_required ? 1: 0
+    count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.main.route_table_id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
+
